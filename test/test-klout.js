bf0e777618ba2d365be13bf0e5672b2f9905780f
Myles Borins <myles@famo.us>
2015-05-24 09:23:24 -0700

    optimize

--- test/test-klout.js
+++ test/test-klout.js
@@ -4,9 +4,9 @@ var test = require('tap').test;
 
 var klout = require('../lib/klout');
 
-test('it can get the kloutid of a user based on twitter ID', function (t) {
+test('it can get the kloutid of a user based on twitter screen name', function (t) {
   t.plan(2);
-  klout.id('150664007', function (err, id) {
+  klout.id('thealphanerd', function (err, id) {
 
     t.error(err, 'no error!!!');
     t.equals(id, '42502726248281752', 'we should get back the id');
